8a2dd6fa8727d17c2ffb782b6e2ec2103e8ad8ec
Hira <HiraShahid2006@gmail.com>
2026-07-05 12:56:40 +0100

    Completed section

--- src/basics/While_Loops.java
+++ src/basics/While_Loops.java
@@ -8,7 +8,7 @@ public class While_Loops {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter your name: ");
+        System.out.print("Please enter your name: ");
         String name = scanner.nextLine();
 
         while(name.isEmpty()){
@@ -21,6 +21,20 @@ public class While_Loops {
             System.out.println("Hello " + name + "!");
         }
 
-    }
+        //a variation of a while loop is a do while loop
+        //you do some code first before checking a condition compared to a while loop where you may skip the code
+        //entirely if the condition is false
 
-}
+        int age = 0;
+
+        do{
+            System.out.println("Your age cannot be a negative!");
+            System.out.print("Enter your age: ");
+            age = scanner.nextInt();
+        }while(age < 0);
+
+        System.out.println("You are " + age + " years old!");
+        scanner.close();
+        }
+
+    }
